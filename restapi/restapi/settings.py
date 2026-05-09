08a94ac944ec6b4fc43c9c5c7f2415d4a43a9bb5
lenova <mihaddeskto@gmail.com>
2026-05-09 17:02:18 +0530

    connected django with postgresql

--- restapi/restapi/settings.py
+++ restapi/restapi/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'restapi.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mydb',
+        'USER': 'postgres',
+        'PASSWORD': 'mihad',
+        'HOST': 'localhost',
+        'PORT': '5433',
     }
 }
 
